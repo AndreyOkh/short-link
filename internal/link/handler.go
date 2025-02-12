d720f915674776868c0aeb569a140cef74fc3305
Andrey <andrey1q2q@gmail.com>
2025-02-12 22:52:18 +1000

    Добавлен функционал удаления ссылки и проверка хэша на существование

--- internal/link/handler.go
+++ internal/link/handler.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"short-link/pkg/req"
 	"short-link/pkg/res"
+	"strconv"
 )
 
 type LinkHandler struct {
@@ -31,6 +32,7 @@ func (handler *LinkHandler) create() http.HandlerFunc {
 			return
 		}
 		link := NewLink(body.URL)
+		link.generateHash(handler)
 		createdLink, err := handler.LinkRepository.Create(link)
 		if err != nil {
 			res.Json(w, "error creating link: "+err.Error(), http.StatusInternalServerError)
@@ -48,13 +50,40 @@ func (handler *LinkHandler) update() http.HandlerFunc {
 
 func (handler *LinkHandler) delete() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		log.Println("Delete request received", id)
+		id, err := strconv.Atoi(r.PathValue("id"))
+		if err != nil {
+			res.Json(w, "invalid id: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+		if err := handler.LinkRepository.DeleteByID(id); err != nil {
+			res.Json(w, "error deleting link: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		res.Json(w, "link deleted", http.StatusOK)
 	}
 }
 
 func (handler *LinkHandler) get() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		hash := r.PathValue("hash")
+		log.Println("Get request received", hash)
+		link, err := handler.LinkRepository.FindByHash(hash)
+		if err != nil {
+			res.Json(w, "error getting link: "+err.Error(), http.StatusNotFound)
+			return
+		}
+		http.Redirect(w, r, link.URL, http.StatusTemporaryRedirect)
+	}
+}
 
+func (link *Link) generateHash(handler *LinkHandler) {
+	for ok := false; !ok; {
+		_, err := handler.LinkRepository.FindByHash(link.Hash)
+		if err != nil && err.Error() == "record not found" {
+			ok = true
+			continue
+		} else {
+			link.Hash = RandStringRunes(6)
+		}
 	}
 }
--- internal/link/repository.go
+++ internal/link/repository.go
@@ -8,10 +8,6 @@ type LinkRepository struct {
 	Database *db.Db
 }
 
-//type LinkRepository struct {
-//	Database *db.Db
-//}
-
 func NewLinkRepository(database *db.Db) *LinkRepository {
 	return &LinkRepository{
 		Database: database,
@@ -23,4 +19,21 @@ func (repo *LinkRepository) Create(link *Link) (*Link, error) {
 		return nil, result.Error
 	}
 	return link, nil
+}
+
+func (repo *LinkRepository) FindByHash(hash string) (*Link, error) {
+	var link Link
+	result := repo.Database.DB.Where("hash = ?", hash).First(&link)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &link, nil
+}
+
+func (repo *LinkRepository) DeleteByID(linkID int) error {
+	result := repo.Database.DB.Delete(&Link{}, linkID)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
 }
